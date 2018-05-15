50487b8a20caa3ea9f83ab18605ef8d0fc96d619
xuserwhat <quicknshell@gmail.com>
2018-05-15 22:50:25 +0900

    Fix license information from sources

--- src/bot.rs
+++ src/bot.rs
@@ -1,8 +1,8 @@
 // =======================================================================
 //  Copyleft 총통각하 2018-∞.
-//  Distributed under the terms of the MIT License.
+//  Distributed under the terms of the 3-Clause BSD License.
 //  (See accompanying file LICENSE or copy at
-//   https://opensource.org/licenses/MIT)
+//   https://opensource.org/licenses/BSD-3-Clause)
 // =======================================================================
 
 //* Use from external library *//
